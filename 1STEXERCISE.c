43208b2c4ede6c0af566afffcdc0a0952dbbf060
Harsh-dev <harshitarora974@gmail.com>
2026-07-09 17:22:42 +0530

    Create 1STEXERCISE.c with coding problems
    Added exercise problems and solutions outline in 1STEXERCISE.c

---
+++ 1STEXERCISE.c
@@ -0,0 +1,34 @@
+/*
+aim - help students to ractise their coding skills , becoming a expert coder 
+*/
+
+*
+ * * * *  * * * *  * * * *  * * * *  *        * * * *  ********  * * * *
+ *     *  *     *  *     *  *     *  *        *        *  **  *  *
+ * * * *  * * * *  *     *  * * * *  *        * * * *  *  **  *  * * * *
+ *        * *      *     *  *     *  *        *        *      *        *
+ *        *   *    * * * *  * * * *  * * * *  * * * *  *      *  * * * *
+
+
+→ PROBLEM NO . 1.1
+  ramesh basic salary is input , his dearness allownance (da) is 40% , house rent allowance (hra) is 20% of basic salary calculate gross salaray (gs)
+→ PROBLEM NO . 1.2
+  to change distance from kilometers (km) into metres (m) , feet (ft) , inches (in) and centimeters (cm)
+→ PROBLEM NO . 1.3
+   marks of a student in 5 subjects is input , calculate the aggregate (aggr) marks and percentage (per) in 5 subjects
+
+
+ * * * *     * * * *  * * * *  * * * *  * * * *  *        * * * *  ********  * * * *
+ *           *     *  *     *  *     *  *     *  *        *        *  **  *  *
+ * * * *     * * * *  * * * *  *     *  * * * *  *        * * * *  *  **  *  * * * *
+ *           *        * *      *     *  *     *  *        *        *      *        *
+ * * * *  *  *        *   *    * * * *  * * * *  * * * *  * * * *  *      *  * * * *
+
+  
+→ EXERCISE PROBLEM 1. 
+    to convert temp from fegree celcius to kelvin
+→ EXERCISE PROBLEM 2.
+    enter length, breadth and radius and calculate rectangle perimeter plus circle area/circumference 
+
+
+→ SOLUTIONS ON " 1STEXSOL.c " 
