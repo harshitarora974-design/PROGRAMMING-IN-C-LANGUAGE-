a0aa9617753519c663f50bfe433e3c50a195c649
Harsh-dev <harshitarora974@gmail.com>
2026-07-09 14:55:59 +0530

    Update INPUTFROMUSER.c

--- INPUTFROMUSER.c
+++ INPUTFROMUSER.c
@@ -6,7 +6,7 @@ aim - to take input from user and do tasks in topic variable
 
 int main()
 
- /*// sum of two nums
+ // sum of two nums
 {
     int a, b, t;
     printf("enter two nums\n");
@@ -15,86 +15,12 @@ int main()
     printf("sum of two nums is %d", t);
     return 0;
 }
-*/
 
+// ASSIGNMENTS 
 
+// 1. subtraction of two nums
+// 2. product of two nums
+// 3. division of two nums
+// 4. modulus of two nums
+// 5. all 5 programms combined in one program
 
-/* // subtraction of two nums
-{
-    int a, b, t;
-    printf("enter two nums\n");
-    scanf("%d%d", &a, &b);
-    t = b - a;
-    printf("subtraction of two nums is %d", t);
-
-    return 0;
-}
-*/
-
-/* // product of two nums
-{
-    int a, b, t;
-    printf("enter two nums\n");
-    scanf("%d%d", &a, &b);
-    t = a * b;
-    printf("product of two nums is %d", t);
-
-    return 0;
-}
-*/
-
-/* // division of two nums
-{
-    int a, b, t;
-    printf("enter two nums\n");
-    scanf("%d%d", &a, &b);
-    t = b / a;
-    printf("division of two nums is %d", t);
-
-    return 0;
-}
-*/
-
-/* // modulus of two nums
-{
-    int a, b, t;
-    printf("enter two nums\n");
-    scanf("%d%d", &a, &b);
-    t = b % a;
-    printf("modulus of two nums is %d", t);
-
-    return 0;
-}
-*/
-
-/*
-   * * * *  * * * *  * * * *  * * * *  *        * * * *  ********  * * * *      
-   *     *  *     *  *     *  *     *  *        *        *  **  *  * 
-   * * * *  * * * *  *     *  * * * *  *        * * * *  *  **  *  * * * *
-   *        * *      *     *  *     *  *        *        *      *        *
-   *        *   *    * * * *  * * * *  * * * *  * * * *  *      *  * * * *  
-*/
-
-/* // all 5 programms combined in one program
-{
-    int a, b, t;
-    printf("enter two nums\n");
-    scanf("%d%d", &a, &b);
-    t = a + b;
-    printf("sum of two nums is %d\n", t);
-
-    t = b - a;
-    printf("subtraction of two nums is %d\n", t);
-
-    t = a * b;
-    printf("product of two nums is %d\n", t);
-
-    t = b / a;
-    printf("division of two nums is %d\n", t);
-
-    t = b % a;
-    printf("modulus of two nums is %d\n", t);
-
-    return 0;
-}
-*/
