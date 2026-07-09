a59fd50bac4ed5a22f0d30b0a1adfbec86f406b9
Harsh-dev <harshitarora974@gmail.com>
2026-07-09 16:29:22 +0530

    Refactor FUNCTIONS.c with function definitions

--- FUNCTIONS.c
+++ FUNCTIONS.c
@@ -1,2 +1,42 @@
 /*
 defination -  Function – Set of Instructions. Part of Program (Sub Program)
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+msg()   // definition
+{
+   printf("\nHello User");
+   printf("\nWelcome in C");
+   //printf("\nI am doing C");
+}
+
+int main()
+{
+   clrscr();
+
+   msg(); // calling
+
+   printf("\nC Develop by Dennis Retchie");
+
+   msg();
+
+   printf("\nC Develop in 1972");
+
+   msg();
+
+   sum(); //call
+
+   getch();
+}
+
+sum()
+{
+  int n1,n2,r;
+  printf("\nenter 2 nos");
+  scanf("%d%d",&n1,&n2);
+  r=n1+n2;
+  printf("\nSum=%d",r);
+
+}
