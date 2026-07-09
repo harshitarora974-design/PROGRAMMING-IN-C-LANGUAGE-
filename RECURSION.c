225f300ab5b1b804a3c4f0a891ea590a7de0fbb4
Harsh-dev <harshitarora974@gmail.com>
2026-07-09 17:00:01 +0530

    Add recursive main function to print 'Hello'
    Implement a recursive function to print 'Hello' ten times.

---
+++ RECURSION.c
@@ -0,0 +1,18 @@
+/*
+Recursion - When a function call itself then that function is known as
+	         recursive function and process is known as recursion
+*/
+
+#include<stdio.h>
+int x=0;
+int main()
+{
+  
+   printf("\nHello");
+   if(x<10)
+   {
+   x++;
+   main(); //recursion
+   }
+   return 0; 
+}
