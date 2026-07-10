ef2a15f04c5e74f6de39c4c0126440fc47662571
Harsh-dev <harshitarora974@gmail.com>
2026-07-10 14:24:38 +0530

    Add coding exercises to 3RDEXERCISE.c
    Added coding exercises and problems for practice.

---
+++ 3RDEXERCISE.c
@@ -0,0 +1,43 @@
+/*
+aim - help students to ractise their coding skills , becoming a expert coder 
+*/
+
+
+ * * * *  * * * *  * * * *  * * * *  *        * * * *  ********  * * * *
+ *     *  *     *  *     *  *     *  *        *        *  **  *  *
+ * * * *  * * * *  *     *  * * * *  *        * * * *  *  **  *  * * * *
+ *        * *      *     *  *     *  *        *        *      *        *
+ *        *   *    * * * *  * * * *  * * * *  * * * *  *      *  * * * *
+
+
+→ PROBLEM NO . 3.1
+   give 10% discount if mrp>1000 otherwise give 5% discountand print the net price after discount
+→ PROBLEM NO . 3.2
+   if bs < 1500 then hra = 10% of bs and da = 90% of bds , is bs => 1500 then hra = 500 rupees (to be added in bs) and da = 98% of bs find gs
+→ PROBLEM NO . 3.3
+   cost price and selling price is input , find whether seller made profit or loss
+→ PROBLEM NO . 3.4
+   if any num (n) is input , check whether num is even or odd
+→ PROBLEM NO . 3.5
+   year is the input , check whether year is leap year or non leap year
+
+
+ * * * *  *     *     * * * *  * * * *  * * * *  * * * *
+ *          * *       *     *  *     *  *     *  *     *  
+ * * * *     *        * * * *  * * * *  *     *  * * * *
+ *          * *       *        * *      *     *  *     *
+ * * * *  *     *  *  *        *   *    * * * *  * * * *
+
+→ EXERCISE PROBLEM 1.
+   5 digits are input , to determine reserved num are equal or not
+→ EXERCISE PROBLEM 2.
+   ram , sham , ajay are input , program to find the youngest of three
+→ EXERCISE PROBLEM 3.
+   program to check triangle is valid or not , sum of three angles = 180
+→ EXERCISE PROBLEM 4.
+   program to find area of rectangle is greater than its perimeter 
+
+
+SOLUTIONS ON " 3RDEXSOL.c "
+
+   
