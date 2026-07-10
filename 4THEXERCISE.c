2a05ba33d6a072557de45b2ebd44ec6c5fef5954
Harsh-dev <harshitarora974@gmail.com>
2026-07-10 14:49:00 +0530

    Create 4THEXERCISE.c with coding problems
    Added initial structure and problem statements for coding exercises.

---
+++ 4THEXERCISE.c
@@ -0,0 +1,41 @@
+/*
+aim - help students to ractise their coding skills , becoming a expert coder 
+*/
+
+
+ * * * *  * * * *  * * * *  * * * *  *        * * * *  ********  * * * *
+ *     *  *     *  *     *  *     *  *        *        *  **  *  *
+ * * * *  * * * *  *     *  * * * *  *        * * * *  *  **  *  * * * *
+ *        * *      *     *  *     *  *        *        *      *        *
+ *        *   *    * * * *  * * * *  * * * *  * * * *  *      *  * * * *
+
+
+→ PROBLEM NO . 4.1
+   marks obtained by student in 5 different subjects . calculate its diviion acc to criteria
+→ PROBLEM NO . 4.2
+   program to calculate salary as per table 
+| **Gender** | **Years of Service** | **Qualifications** | **Salary** |
+| ---------- | -------------------- | ------------------ | ---------: |
+| Male       | >= 10                | Post-Graduate      |      11000 |
+| Male       | >= 10                | Graduate           |      10000 |
+| Male       | < 10                 | Post-Graduate      |      10000 |
+| Male       | < 10                 | Graduate           |       7000 |
+| Female     | >= 10                | Post-Graduate      |      12000 |
+| Female     | >= 10                | Graduate           |       9000 |
+| Female     | < 10                 | Post-Graduate      |      10000 |
+| Female     | < 10                 | Graduate           |       6000 |
+
+
+ * * * *  *     *     * * * *  * * * *  * * * *  * * * *
+ *          * *       *     *  *     *  *     *  *     *  
+ * * * *     *        * * * *  * * * *  *     *  * * * *
+ *          * *       *        * *      *     *  *     *
+ * * * *  *     *  *  *        *   *    * * * *  * * * *
+
+
+ → EXERCISE PROBLEM 1.
+   length of three sides of triangle is input, find if it is equilateral, isosceles, or scalen
+
+SOLUTIONS ON " 4THEXSOL.c "
+
+   
