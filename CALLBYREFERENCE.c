17faaf065636fb66fe29ed5197f3a3804ff7c9d6
Harsh-dev <harshitarora974@gmail.com>
2026-07-12 22:30:43 +0530

    Update CALLBYREFERENCE.c

--- CALLBYREFERENCE.c
+++ CALLBYREFERENCE.c
@@ -1,8 +1,6 @@
-/*
-Call By Reference
-if any change in formal parameter it 
-affects actual  paramater
-*/
+
+→ TOPIC -Call By Reference
+          if any change in formal parameter it affects actual  paramater
 
 #include<stdio.h>
 
@@ -20,6 +18,7 @@ void fun(int *x)  // x is formal parameter
 {
   *x=100;
 }
+
 /*
 Swaping of 2 nos using call by reference
 */
