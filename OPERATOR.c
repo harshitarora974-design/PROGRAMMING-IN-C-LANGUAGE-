f091eb0a75877edd9419171e2a7534517397aa80
Harsh-dev <harshitarora974@gmail.com>
2026-07-12 22:43:59 +0530

    Refactor comments and structure in OPERATOR.c

--- OPERATOR.c
+++ OPERATOR.c
@@ -1,20 +1,19 @@
-/*
-aim - operators
-defination - operators are symbols which are used to perform operations on operands.
-types of operators - 1. arithmetic operators
-                  2. relational operators
-                  3. logical operators
-                  4. bitwise operators
-                  5. assignment operators
-                  6. miscellaneous operators
- */
 
-// use of increment x++/++x
+→ AIM - operators
+→ DEFINATION - operators are symbols which are used to perform operations on operands.
+→ Types of operators - 1. arithmetic operators
+                       2. relational operators
+                       3. logical operators
+                       4. bitwise operators
+                       5. assignment operators
+                       6. miscellaneous operators
+
+→ use of increment x++/++x
 
 #include <stdio.h>
 
 int main()
-    /*
+ 
     { // for one char only
         int x = 5;
         x++; // post increment
@@ -22,9 +21,9 @@ int main()
 
         return 0;
     }
-   */
+ 
 
-    /*
+ 
     { // for two different chars
         int x = 5, y = 10;
         x++; // post increment
@@ -36,7 +35,7 @@ int main()
         return 0;
 
     }
-    */
+ 
 
     /*
     some imp terms
